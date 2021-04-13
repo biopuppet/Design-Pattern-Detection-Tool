824b5faf5ff8b2ec223757fa31330456b1fcd7b2
biopuppet <biopuppet@outlook.com>
2021-04-13 12:57:29 +0800

    Add all sub-patterns

--- src/sub_pattern.hpp
+++ src/sub_pattern.hpp
@@ -9,14 +9,14 @@ enum SubPatternType {
     SPT_CI,
     SPT_IAGG,
     SPT_IPAG,
-    SPT_MLI,
-    SPT_IASS,
-    SPT_SAGG,
-    SPT_IIAGG,
+    SPT_IPAS,
     SPT_SASS,
+    SPT_MLI,
+    SPT_IIAGG,
+    SPT_IASS,
     SPT_ICD,
     SPT_DCI,
-    SPT_IPAS,
+    SPT_SAGG,
     SPT_AGPI,
     SPT_IPD,
     SPT_DPI,
@@ -82,12 +82,142 @@ public:
     }
 };
 
+class IPAG : public SubPattern {
+public:
+    IPAG() : SubPattern("IPAG", 3) {
+        g.edge(1, 0) = Relation::Inheritance;
+        g.edge(2, 0) = Relation::Aggregation;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_IPAG;
+    }
+};
+
+class IPAS : public SubPattern {
+public:
+    IPAS() : SubPattern("IPAS", 3) {
+        g.edge(0, 2) = Relation::Association;
+        g.edge(1, 0) = Relation::Inheritance;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_IPAS;
+    }
+};
+
 class SASS : public SubPattern {
 public:
     SASS() : SubPattern("SASS", 1) { g.edge(0, 0) = Relation::Association; }
 
     const SubPatternType type() const override {
         return SubPatternType::SPT_SASS;
+    }
+};
+
+class MLI : public SubPattern {
+public:
+    MLI() : SubPattern("MLI", 3) {
+        g.edge(1, 0) = Relation::Inheritance;
+        g.edge(2, 1) = Relation::Inheritance;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_MLI;
+    }
+};
+
+class IIAGG : public SubPattern {
+public:
+    IIAGG() : SubPattern("IIAGG", 3) {
+        g.edge(1, 0) = Relation::Inheritance;
+        g.edge(2, 1) = Relation::Inheritance;
+        g.edge(0, 2) = Relation::Aggregation;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_IIAGG;
+    }
+};
+
+class IASS : public SubPattern {
+public:
+    IASS() : SubPattern("IASS", 2) {
+        g.edge(1, 0) = Relation::Inheritance;
+        g.edge(1, 0) = Relation::Association;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_IASS;
+    }
+};
+
+class ICD : public SubPattern {
+public:
+    ICD() : SubPattern("ICD", 3) {
+        g.edge(1, 0) = Relation::Inheritance;
+        g.edge(1, 2) = Relation::Dependency;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_ICD;
+    }
+};
+
+class DCI : public SubPattern {
+public:
+    DCI() : SubPattern("DCI", 3) {
+        g.edge(1, 0) = Relation::Inheritance;
+        g.edge(2, 1) = Relation::Dependency;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_DCI;
+    }
+};
+
+class SAGG : public SubPattern {
+public:
+    SAGG() : SubPattern("SAGG", 1) { g.edge(0, 0) = Relation::Aggregation; }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_SAGG;
+    }
+};
+
+class AGPI : public SubPattern {
+public:
+    AGPI() : SubPattern("AGPI", 3) {
+        g.edge(0, 2) = Relation::Aggregation;
+        g.edge(1, 0) = Relation::Inheritance;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_AGPI;
+    }
+};
+
+class IPD : public SubPattern {
+public:
+    IPD() : SubPattern("IPD", 3) {
+        g.edge(0, 2) = Relation::Dependency;
+        g.edge(1, 0) = Relation::Inheritance;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_IPD;
+    }
+};
+
+class DPI : public SubPattern {
+public:
+    DPI() : SubPattern("DPI", 3) {
+        g.edge(2, 0) = Relation::Dependency;
+        g.edge(1, 0) = Relation::Inheritance;
+    }
+
+    const SubPatternType type() const override {
+        return SubPatternType::SPT_DPI;
     }
 };
 
